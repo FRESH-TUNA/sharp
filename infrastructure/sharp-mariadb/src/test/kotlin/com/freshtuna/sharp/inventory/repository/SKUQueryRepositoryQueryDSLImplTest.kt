4052b19fb9cbfdf5a87ee48756c7cbfeaf201792
Dongwon Kim <freshtuna@kakao.com>
2023-03-28 14:41:08 +0900

    (테스트) SKU 검색 어댑터 테스트 작성

--- infrastructure/sharp-mariadb/src/test/kotlin/com/freshtuna/sharp/inventory/repository/SKUQueryRepositoryQueryDSLImplTest.kt
+++ infrastructure/sharp-mariadb/src/test/kotlin/com/freshtuna/sharp/inventory/repository/SKUQueryRepositoryQueryDSLImplTest.kt
@@ -23,13 +23,15 @@ class SKUQueryRepositoryQueryDSLImplTest {
     private lateinit var skuQueryRepository: SKUQueryRepositoryQueryDSLImpl
 
     @Test
-    @DisplayName("페이지의 사이즈대로 잘 검색되는지 테스트")
-    fun searchTest() {
+    @DisplayName("페이지 테스트")
+    fun pageTest() {
         /**
          * given
          */
         val pageSize = 2L
-        val command = SearchSkuCommand(sharpPageRequest = SharpPageRequest(0, pageSize, SharpSort()))
+        val pageNumber = 1L
+        val totalCount = 5L
+        val command = SearchSkuCommand(sharpPageRequest = SharpPageRequest(pageNumber, pageSize, SharpSort()))
 
         /**
          * when
@@ -39,6 +41,56 @@ class SKUQueryRepositoryQueryDSLImplTest {
         /**
          * then
          */
+        assertEquals(pageNumber, results.pageNumber)
         assertEquals(pageSize, results.count)
+
+        assertEquals(totalCount, results.totalCount)
+        assertEquals(totalCount/pageSize, results.totalPageCount)
+    }
+
+    @Test
+    @DisplayName("sku 이름으로 쿼리 테스트")
+    fun queryNameTest() {
+        /**
+         * given
+         */
+        val pageSize = 2L
+        val command = SearchSkuCommand(
+            query = "파카리",
+            sharpPageRequest = SharpPageRequest(0, pageSize, SharpSort())
+        )
+
+        /**
+         * when
+         */
+        val results = skuQueryRepository.search(command)
+
+        /**
+         * then
+         */
+        assertEquals(true, results.data.isNotEmpty())
+    }
+
+    @Test
+    @DisplayName("sku 바코드로 쿼리 테스트")
+    fun queryBarcodeTest() {
+        /**
+         * given
+         */
+        val pageSize = 2L
+        val command = SearchSkuCommand(
+            query = "234523",
+            sharpPageRequest = SharpPageRequest(0, pageSize, SharpSort())
+        )
+
+        /**
+         * when
+         */
+        val results = skuQueryRepository.search(command)
+
+        /**
+         * then
+         */
+        assertEquals(true, results.data.isNotEmpty())
     }
 }
