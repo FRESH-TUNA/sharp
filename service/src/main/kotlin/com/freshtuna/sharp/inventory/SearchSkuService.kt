3d8cee67c6ea5294cda532330f91dfd0024f0056
Dongwon Kim <freshtuna@kakao.com>
2023-03-28 16:03:30 +0900

    (기능) 서비스 계층 필요 어노테이션 추가

--- service/src/main/kotlin/com/freshtuna/sharp/inventory/SearchSkuService.kt
+++ service/src/main/kotlin/com/freshtuna/sharp/inventory/SearchSkuService.kt
@@ -3,7 +3,9 @@ package com.freshtuna.sharp.inventory
 import com.freshtuna.sharp.inventory.command.SearchSkuCommand
 import com.freshtuna.sharp.inventory.incoming.SearchSkuUseCase
 import com.freshtuna.sharp.inventory.outgoing.SearchSkuPort
+import org.springframework.stereotype.Service
 
+@Service
 class SearchSkuService(
     private val searchSkuPort: SearchSkuPort
 ) : SearchSkuUseCase {
--- service/src/main/kotlin/com/freshtuna/sharp/inventory/StockInService.kt
+++ service/src/main/kotlin/com/freshtuna/sharp/inventory/StockInService.kt
@@ -3,7 +3,11 @@ package com.freshtuna.sharp.inventory
 import com.freshtuna.sharp.inventory.command.StockInCommand
 import com.freshtuna.sharp.inventory.incoming.StockInUseCase
 import com.freshtuna.sharp.inventory.outgoing.StockInPort
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
+@Service
+@Transactional
 class StockInService(
     private val stockInPort: StockInPort
 ) : StockInUseCase {
