06a32389422e314cfb55bcc92ee9b431166234fa
Dongwon Kim <freshtuna@kakao.com>
2023-10-23 17:49:49 +0900

    1.1.2

--- api/inventory-api/src/main/kotlin/com/freshtuna/sharp/config/WebMVCConfig.kt
+++ api/inventory-api/src/main/kotlin/com/freshtuna/sharp/config/WebMVCConfig.kt
@@ -4,11 +4,10 @@ import com.freshtuna.sharp.config.env.CORSProperties
 import com.freshtuna.sharp.config.intercepter.SellerAuthInterceptor
 import com.freshtuna.sharp.config.resolver.SharpIDResolver
 import io.github.oshai.KotlinLogging
-import org.springframework.context.annotation.Bean
+
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -31,18 +30,17 @@ class WebMVCConfig(
         argumentResolvers.add(sharpIDResolver)
     }
 
-    /** CORS 설정  */
-    @Bean
-    fun corsConfigurationSource(): UrlBasedCorsConfigurationSource {
-        val corsConfigSource = UrlBasedCorsConfigurationSource()
-        val corsConfig = CorsConfiguration()
-        corsConfig.allowedHeaders = corsProperties.allowedHeaders.split(",")
-        corsConfig.allowedMethods = corsProperties.allowedMethods.split(",")
-        corsConfig.allowedOrigins = corsProperties.allowedOrigins.split(",")
-        corsConfig.exposedHeaders = corsProperties.exposedHeaders.split(",")
-        corsConfig.maxAge = corsProperties.maxAge
-        corsConfig.allowCredentials = true
-        corsConfigSource.registerCorsConfiguration("/**", corsConfig)
-        return corsConfigSource
+    /**
+     * CORS 설정
+     */
+    override fun addCorsMappings(registry: CorsRegistry) {
+
+        registry.addMapping("/**")
+            .allowedOrigins(*corsProperties.allowedOriginArray())
+            .allowedMethods(*corsProperties.allowedMethodArray())
+            .allowedHeaders(*corsProperties.allowedHeaderArray())
+            .exposedHeaders(*corsProperties.exposedHeaderArray())
+            .allowCredentials(true)
+            .maxAge(corsProperties.maxAge);
     }
 }
